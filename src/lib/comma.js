fcfa96326d0381f4fc12fda4534d1ff40da529cf
yeoseoyun <yeosummer94@gmail.com>
2024-04-30 13:40:43 +0900

    [Design] :   swiper button 색상 수정

--- src/lib/comma.js
+++ src/lib/comma.js
@@ -1,4 +1,4 @@
 export default function comma(number) {
   number = Math.floor(number / 10) * 10;
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
